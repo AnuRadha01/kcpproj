fc574b33e8569e53c2f23e3553b7a7c746c2578b
Subba Rao Jadda <rsubba@kodiakptt.com>
2018-08-27 11:25:18 +0530

    New project added

---
+++ DevOpsKcp/home.js
@@ -0,0 +1,3 @@
+﻿function getFullName(){
+    document.getElementById('fullname').value = document.getElementById('fname').value + ' ' + document.getElementById('mname').value + ' ' + document.getElementById('lname').value;
+}
